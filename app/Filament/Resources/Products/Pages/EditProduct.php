e859a2c409c96877adcb2fdafb57a663cb0c6fe1
Иван <ano101@mail.ru>
2026-02-12 23:59:13 +0300

    доп

--- app/Filament/Resources/Products/Pages/EditProduct.php
+++ app/Filament/Resources/Products/Pages/EditProduct.php
@@ -3,10 +3,12 @@
 namespace App\Filament\Resources\Products\Pages;
 
 use App\Filament\Resources\Products\ProductResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Icons\Heroicon;
 
 class EditProduct extends EditRecord
 {
@@ -15,6 +17,12 @@ class EditProduct extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('view')
+                ->label('Перейти на товар')
+                ->icon(Heroicon::OutlinedEye)
+                ->url(fn () => $this->record->getSlug() ? route('product.show', ['slug' => $this->record->getSlug()]) : null)
+                ->openUrlInNewTab()
+                ->hidden(fn () => ! $this->record->getSlug()),
             DeleteAction::make(),
             ForceDeleteAction::make(),
             RestoreAction::make(),
